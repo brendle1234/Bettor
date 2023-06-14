53053bbfdcd540062b2c88e6300f7e1308a0f1de
brendle1234 <95358979+brendle1234@users.noreply.github.com>
2023-06-14 06:14:59 -0400

    Update XGBoost_Runner.py

--- src/Predict/XGBoost_Runner.py
+++ src/Predict/XGBoost_Runner.py
@@ -1,9 +1,16 @@
 import copy
+
 import numpy as np
+import pandas as pd
 import xgboost as xgb
+from colorama import Fore, Style, init, deinit
 from src.Utils import Expected_Value
 from src.Utils import Kelly_Criterion as kc
 
+
+# from src.Utils.Dictionaries import team_index_current
+# from src.Utils.tools import get_json_data, to_data_frame, get_todays_games_json, create_todays_games
+init()
 xgb_ml = xgb.Booster()
 xgb_ml.load_model('Models/XGBoost_Models/XGBoost_68.6%_ML-2.json')
 xgb_uo = xgb.Booster()
@@ -72,11 +79,14 @@ def xgb_runner(data, todays_games_uo, frame_ml, games, home_team_odds, away_team
         if home_team_odds[count] and away_team_odds[count]:
             ev_home = float(Expected_Value.expected_value(ml_predictions_array[count][0][1], int(home_team_odds[count])))
             ev_away = float(Expected_Value.expected_value(ml_predictions_array[count][0][0], int(away_team_odds[count])))
-
+        expected_value_colors = {'home_color': Fore.GREEN if ev_home > 0 else Fore.RED,
+                        'away_color': Fore.GREEN if ev_away > 0 else Fore.RED}
         bankroll_descriptor = ' Fraction of Bankroll: '
         bankroll_fraction_home = bankroll_descriptor + str(kc.calculate_kelly_criterion(home_team_odds[count], ml_predictions_array[count][0][1])) + '%'
         bankroll_fraction_away = bankroll_descriptor + str(kc.calculate_kelly_criterion(away_team_odds[count], ml_predictions_array[count][0][0])) + '%'
 
-        print(home_team + ' EV: ' + str(ev_home) + (bankroll_fraction_home if kelly_criterion else ''))
-        print(away_team + ' EV: ' + str(ev_away) + (bankroll_fraction_away if kelly_criterion else ''))
+        print(home_team + ' EV: ' + expected_value_colors['home_color'] + str(ev_home) + Style.RESET_ALL + (bankroll_fraction_home if kelly_criterion else ''))
+        print(away_team + ' EV: ' + expected_value_colors['away_color'] + str(ev_away) + Style.RESET_ALL + (bankroll_fraction_away if kelly_criterion else ''))
         count += 1
+
+    deinit()
